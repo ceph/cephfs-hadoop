5319432069cb4e2754d6f99e1e0660ca6c42b820
jayunit100 <jayunit100@gmail.com>
2014-07-22 00:38:55 -0400

    Vagrant recipe for Ceph , first pass

---
+++ resources/vagrant/ceph-install.sh
@@ -0,0 +1,8 @@
+  sudo yum update && sudo yum install ceph-deploy
+  yum install -y ceph
+  yum install -y ceph-release
+  yum install -y ceph-deploy
+  yum install -y ceph-common
+  yum install -y ceph-devel
+  yum install -y ceph-fuse
+  yum install -y ceph-fuse libcephfs1.x86_64 rbd-fuse 
